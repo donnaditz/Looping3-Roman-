da857d561f58c0f2b7db1c112cadae28c8fba7ea
Gigi Ditz and Inthrayuth Mahaphol <gigi@>
2016-03-31 14:02:56 -0700

    Modify Roman Numeral script

--- js/scripts.js
+++ js/scripts.js
@@ -8,25 +8,30 @@ $(document).ready(function() {
     var number1 = parseInt($("#intNum").val());
     // var roman = ["I","IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"];
     // var arabic = ["1","4", "5", "9", "10", "40", "50", "90", "100", "400", "500", "900", "1000"]
-   var roman = ["X","IX", "V", "IV", "I"]
-   var arabic = [10, 9, 5, 4, 1]
-
+    var roman = ["X", "V", "I"];
+    var arabic = [10, 5, 1];
+    var tempNum = number1;
     var result = [];
 
     if (number1 < 1 || number1 > 3999) {
       $("#not").text("Out of Range");
     }
-    else if (number1 <= 10) {
-      for (i = 0; i <= 5; i += 1) {
-        if (number1 % arabic[i] === 0) {
-          result.push(roman[i]);
-          console.log(result);
+    else {
+      for (i = 0; i < arabic.length; i += 1) {     // This loop to give the subtract number from the array.
+        console.log(i);
+        for (j = number1; j >= 0 ; j -= 1) {     //  This loop for subtracting number untill the number can't be subtracted. And go out of the loop.
+          if (number1 >= arabic[i]) {
+            number1 = number1 - arabic[i];
+            result.push(roman[i]);
+          }
+          else
           {break}
         }
-
+console.log(result);
       }
 
     }
+
 
   });
 });

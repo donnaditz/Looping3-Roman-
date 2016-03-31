ce465bb76db7e771cd20a89dd005f12d52d86870
Gigi Ditz and Inthrayuth Mahaphol <gigi@>
2016-03-31 15:47:45 -0700

    modify script to make conveter function

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,10 @@
-//Start Business Logic
-
-
 //Start Interface Logic
 $(document).ready(function() {
   $("#blanks").submit(function(event) {
     event.preventDefault();
     var number1 = parseInt($("#intNum").val());
-    // var roman = ["I","IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"];
-    // var arabic = ["1","4", "5", "9", "10", "40", "50", "90", "100", "400", "500", "900", "1000"]
-    var roman = ["X", "V", "I"];
-    var arabic = [10, 5, 1];
-    var tempNum = number1;
+    var roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V","IV", "I"];
+    var arabic = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
     var result = [];
 
     if (number1 < 1 || number1 > 3999) {
@@ -18,20 +12,16 @@ $(document).ready(function() {
     }
     else {
       for (i = 0; i < arabic.length; i += 1) {     // This loop to give the subtract number from the array.
-        console.log(i);
         for (j = number1; j >= 0 ; j -= 1) {     //  This loop for subtracting number untill the number can't be subtracted. And go out of the loop.
           if (number1 >= arabic[i]) {
             number1 = number1 - arabic[i];
-            result.push(roman[i]);
+            result.push(roman[i]);     //  This will push the letter the result array.
           }
           else
           {break}
         }
-console.log(result);
       }
-
     }
-
-
+    $("#answer").text(result.join(""));
   });
 });
